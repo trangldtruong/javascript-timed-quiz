f4858a2c35a6cbed845b9864f4017a7c685e17b5
Trang Truong <trangldtruong@gmail.com>
2023-09-18 16:12:03 -0400

    question and answer content added

--- script.js
+++ script.js
@@ -16,3 +16,56 @@ function sendMessage() {
 
 }
 setTime();
+
+const questions = [
+    {
+        question: "Which of the following methods is used to access HTML element using Javascript?",
+        answers: [
+            {text: "A.getElementbyId()", correct: false},
+            {text: "B.getElementsByClassName()", correct: false},
+            {text: "C.Both A and B", correct: true},
+            {text: "D.None of the above", correct: false},
+        ]
+    },
+    {
+        question: "What is JavaScript?",
+        answers: [
+            {text: "A.JavaScript is a scripting language used to make the website interactive", correct: true},
+            {text: "B.JavaScript is an assembly language used to make the website interactive", correct: false},
+            {text: "C.JavaScript is a compiled language used to make the website interactive", correct: false},
+            {text: "D.None of the above". correct: false},
+        ]
+    },
+    {
+        question: "Arrays in JavaScript are defined by which of the following statements?",
+        answers: [
+            {text: "A.It is an ordered list of values", correct: true },
+            {text: "B.It is an ordered list of objects", correct: false},
+            {text: "C.It is an ordered list of string", correct: false},
+            {text: "D.It is an ordered list of functions", correct: false},
+        ]
+    },
+    {
+        question: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+        answers: [
+            {text: "A.Position", correct: false},
+            {text: "B.Window", correct: true},
+            {text: "C.Standard", correct: false},
+            {text: "D.Location", correct: false},
+        ]
+    },
+    {
+        question: "Which is the correct jQuery selector to select current HTML element?",
+        answers: [
+            {text: "A.$(this)", correct: true},
+            {text: "B.$(cur)", correct: false},
+            {text: "C.$(.this)", correct: false},
+            {text: "D.$(#this)", correct: false}
+        ]
+    }
+];
+const questionElement = document.getElementById("questions")
+const answerButton = document.getElementById("answer-buttons")
+let currentQuestionIndex = 0;
+let score = 0;
+ 
