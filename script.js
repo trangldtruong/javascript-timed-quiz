32c2ba1ad73b43466679eb3ef3e1edbb06f29432
Trang Truong <trangldtruong@gmail.com>
2023-09-18 22:22:03 -0400

    Next button added

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ function sendMessage() {
 }
 setTime();
 
+const questionElement = document.getElementById("questions")
+const answerButton = document.getElementById("choice")
+var nextButton = document.getElementById("next-button")
 const questions = [
     {
         question: "Which of the following methods is used to access HTML element using Javascript?",
@@ -33,7 +36,7 @@ const questions = [
             {text: "A.JavaScript is a scripting language used to make the website interactive", correct: true},
             {text: "B.JavaScript is an assembly language used to make the website interactive", correct: false},
             {text: "C.JavaScript is a compiled language used to make the website interactive", correct: false},
-            {text: "D.None of the above". correct: false},
+            {text: "D.None of the above", correct: false},
         ]
     },
     {
@@ -64,8 +67,76 @@ const questions = [
         ]
     }
 ];
-const questionElement = document.getElementById("questions")
-const answerButton = document.getElementById("answer-buttons")
-let currentQuestionIndex = 0;
-let score = 0;
- 
+
+function startQuiz (){
+    currentQuestionIndex = 0;
+    Score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+
+}
+
+function showQuestion() {
+    resetState()
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNumber = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("answer-button");
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+    });
+
+}
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+    if(isCorrect){
+        selectedButton.classList.add("correct");
+        Score++;
+
+    }else  {
+        selectedButton.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+
+        } button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+function showScore(){
+    resetState();
+    questionElement.innerHTML = 'Your score';
+    nextButton.innerHTML = "Restart"
+    nextButton.style.display = "block"
+}
+function nextQuestion(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+nextButton.addEventListener("click", ()=> {
+    if(currentQuestionIndex < questions.length){
+        nextQuestion();
+    }else {
+        startQuiz();
+    }
+})
+startQuiz();
